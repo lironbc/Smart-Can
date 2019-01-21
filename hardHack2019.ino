05a00f2e5ca4816cd9d579403bd0f42c08ebbca6
lironbc <36120346+lironbc@users.noreply.github.com>
2019-01-20 19:03:33 -0800

    Updated comments

--- hardHack2019.ino
+++ hardHack2019.ino
@@ -1,14 +1,22 @@
-#include <Wire.h>
+/*
+ * Author: Liron Borwick-Cooper
+ * Date: January 20th, 2019
+ * Resources: http://www.circuitbasics.com/how-to-set-up-an-ultrasonic-range-finder-on-an-arduino/
+ * https://www.arduino.cc/en/Tutorial/LiquidCrystalDisplay
+ */
+  
+  #include <Wire.h>
   #include <LiquidCrystal_I2C.h>
   LiquidCrystal_I2C lcd(0x3F, 2, 1, 0, 4, 5, 6, 7, 3, POSITIVE);  // Set the LCD I2C address, if it's not working try 0x27.   
 
-int trashDepth;
-boolean isClean;
-int startCount; //where we begin counting from for timing
-boolean firstRun = true;
+
+  int trashDepth;
+  boolean isClean;
+  int startCount; //where we begin counting from for timing
+  float duration, distance;
 
 void setup() {
-  // put your setup code here, to run once:
+  
   #define TRIG_PIN 3 
   #define ECHO_PIN 4
   #define TRIG_DELAY 10
@@ -25,15 +33,24 @@ void setup() {
   lcd.print("Initializing");
   
 
-  isClean = true;
-  trashDepth = 100;
+   isClean = true;
+   
+    //Reads initial trash depth
+    digitalWrite(TRIG_PIN, LOW); //Resets the trigger
+    delayMicroseconds(2);
+
+   //Read ultrasonic range finder documentation for explanation on this
+   digitalWrite(TRIG_PIN, HIGH);
+   delayMicroseconds(TRIG_DELAY);
+   digitalWrite(TRIG_PIN, LOW);
+
+  duration = pulseIn(ECHO_PIN,HIGH);
+  trashDepth = duration / 58;
   startCount = 0;
 
 }
 
 void loop() {
-  // put your main code here, to run repeatedly:
-  float duration, distance;
   int percentFull;
   
   digitalWrite(TRIG_PIN, LOW);
@@ -45,23 +62,20 @@ void loop() {
 
   duration = pulseIn(ECHO_PIN,HIGH);
   distance = duration / 58;
-  if(firstRun == true){
-    trashDepth = distance ; 
-    lcd.clear();
-   }
-  distance =distance -3;  
-  if(distance > trashDepth){
+
+  //Set max a min distances because rangefinder occasionally gives garbage values
+    if (distance < 0) {
+      distance = 0;
+    }
+    else if(distance > trashDepth){
     distance = trashDepth;
-  }
-  else if (distance < 0) {
-    distance = 0;
-  }
-  
-  if(firstRun == false){
+    }
+ 
+    lcd.clear();
     lcd.setCursor(0, 0); 
     percentFull = trashDepth - distance;
     percentFull = (int)(percentFull * 100 / trashDepth);
-    lcd.print("                   ");
+    lcd.print("                   "); //Clears the first line
 
     if(percentFull > 80){
       lcd.setCursor(0, 0); 
@@ -72,15 +86,5 @@ void loop() {
     lcd.print(percentFull);
     lcd.print(" % full");
     }
-
-//    lcd.setCursor(0,1);
-//    lcd.print("full");
-  }
-
-
-firstRun=false;
-
-//  Serial.print(distance);
-//  Serial.println(" cm");
-delay(2000);
+delay(10000);
 }
